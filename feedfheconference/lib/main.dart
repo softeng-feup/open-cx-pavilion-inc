e767d99a0687db69c090d434c884ff606ba78c38
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-10-25 16:53:54 +0100

    Bottom Menu

--- feedfheconference/lib/main.dart
+++ feedfheconference/lib/main.dart
@@ -14,6 +14,119 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Destination {
+  const Destination(this.title, this.icon, this.color);
+  final String title;
+  final IconData icon;
+  final MaterialColor color;
+}
+
+const List<Destination> allDestinations = <Destination>[
+  Destination('Home', Icons.home, Colors.teal),
+  Destination('Favorites', Icons.favorite, Colors.cyan),
+  Destination('Form', Icons.assessment, Colors.orange),
+  Destination('Comments', Icons.question_answer, Colors.blue)
+];
+
+
+class DestinationView extends StatefulWidget {
+  const DestinationView({ Key key, this.destination }) : super(key: key);
+
+  final Destination destination;
+
+  @override
+  _DestinationViewState createState() => _DestinationViewState();
+}
+
+class _DestinationViewState extends State<DestinationView> {
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(
+      text: 'sample text: ${widget.destination.title}',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${widget.destination.title} Text'),
+        backgroundColor: widget.destination.color,
+      ),
+      backgroundColor: widget.destination.color[100],
+      body: Container(
+        padding: const EdgeInsets.all(32.0),
+        alignment: Alignment.center,
+        child: TextField(controller: _textController),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+}
+
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -25,21 +138,40 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
+  int _currentIndex = 0;
   TabController _tabController;
   ScrollController _scrollViewController;
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      // Added
-        length: 2, // Added
-        initialIndex: 0, //Added
-        child: Scaffold(
-          body: HomePage(
-              context, _tabController, _scrollViewController, widget.title),
-          drawer: sideDrawer(context), // Passed BuildContext in function.
-        ));
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        child: IndexedStack(
+          index: _currentIndex,
+          children: allDestinations.map<Widget>((Destination destination) {
+            return DestinationView(destination: destination);
+          }).toList(),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: allDestinations.map((Destination destination) {
+          return BottomNavigationBarItem(
+              icon: Icon(destination.icon),
+              backgroundColor: destination.color,
+              title: Text(destination.title)
+          );
+        }).toList(),
+      ),
+    );
   }
+
 }
 
 NestedScrollView HomePage(BuildContext context, TabController _tabController,
@@ -125,6 +257,7 @@ Drawer sideDrawer(BuildContext context) {
             // Update the state of the app
             // ...
             // Then close the drawer
+            MyCustomForm();
             Navigator.pop(context);
           },
         ),
