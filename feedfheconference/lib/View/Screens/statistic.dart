cc4e41f358525ec54c5ef7818ca2982e5ad6a37d
Joao Lirio <up201705254@fe.up.pt>
2019-12-04 10:13:55 +0000

    Add page

--- feedfheconference/lib/View/Screens/statistic.dart
+++ feedfheconference/lib/View/Screens/statistic.dart
@@ -3,55 +3,113 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import '../../Model/db.dart';
 
-class StatisticsForForm extends StatefulWidget {
+class StatisticsForm extends StatefulWidget {
+  final String talkName;
+  final int formId;
+
   @override
-  _LoginPageState createState() => _LoginPageState();
+  StatisticsForm(  { Key key, this.formId, this.talkName}) : super(key: key);
+  _StatisticsFormState createState() => _StatisticsFormState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
-  String _password;
-  String _emailOrUsername;
+class _StatisticsFormState extends State<StatisticsForm> {
+  TabController _tabController;
+  ScrollController _scrollViewController;
 
   @override
   Widget build(BuildContext context) {
-
-
-class StatisticsForForm extends StatelessWidget {
-
-  int formId;
-
-  StatisticsForForm(formId);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
-          ),
-          child: Stack(
-            children: <Widget>[
-              TitleHome('FEEDTHECONFERENCE'),
-              MyButton(
-                  x: 25,
-                  y: 60,
-                  title: "Login",
-                  onPressed: () {
-                    Navigator.of(context).pushNamed("/login");
-                  }),
-              MyButton(
-                  x: 25,
-                  y: 75,
-                  title: "Sign Up",
-                  onPressed: () {
-                    Navigator.of(context).pushNamed("/register");
-                  })
-            ],
-          ),
-        ),
+    return DefaultTabController(
+      child: Scaffold(
+        body: buildStatisticsForm(context, _tabController, _scrollViewController, widget.formId, widget.talkName),
       ),
     );
   }
+}
+
+List<Widget> listMyQuestions(var formId, String talkName) {
+  @override
+  List<Widget> listQuestion = new List();
+
+  int index = 0;
+
+  for(int i = 0; i < db.formList.length; i++){
+    if(db.formList[i].id == formId){
+      FormTalk form = db.formList[i];
+      for(int h = 0; h < form.listIdFormQuestions.length; h++){
+        for(int j = 0; j < db.formQuestionList.length; j++){
+          if(form.listIdFormQuestions[h] == db.formQuestionList[j].id){
+            index++;
+            listQuestion.add(QuestionText(db.formQuestionList[j].questionText, j, talkName, index));
+            break;
+          }
+        }
+      }
+      break;
+    }
+  }
+  return listQuestion;
+}
+
+class QuestionText extends StatelessWidget {
+  final String questionText;
+  final int questionId;
+  final String talkName;
+  final int index;
+  QuestionText(this.questionText, this.questionId, this.talkName, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                questionText,
+                style: TextStyle(fontSize: 22),
+              )
+            ],
+          ),
+          margin: const EdgeInsets.only(bottom: 8.0)
+        //color: Colors.blue[100],
+      ),
+      onTap: () {
+        var route = MaterialPageRoute(
+          builder: (BuildContext context) => new QuestionStatisticsPage(talkName: this.talkName, index: this.index, questionId: questionId)
+        );
+        Navigator.of(context).push(route);
+      },
+    );
+  }
+}
+
+NestedScrollView buildStatisticsForm(
+    BuildContext context,
+    TabController _tabController,
+    ScrollController _scrollViewController,
+    int formId,
+    String talkName) {
+  return new NestedScrollView(
+    controller: _scrollViewController,
+    headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+      return <Widget>[
+        new SliverAppBar(
+          title: new Text(talkName + '\nQuestions'),
+        )
+      ];
+    },
+    body: new Scaffold(
+      body: Container(
+          margin: const EdgeInsets.all(20.0),
+          //color: Colors.amber[600],
+          child: SingleChildScrollView(
+              padding: const EdgeInsets.all(6),
+              child: new Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                  children: listMyQuestions(formId, talkName)
+              )
+          )
+      ),
+    ),
+  );
 }
