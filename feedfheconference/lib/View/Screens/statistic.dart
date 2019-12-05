8fb56485420358e14fc6cf7ac1c59c52a916e9f5
Joao Lirio <up201705254@fe.up.pt>
2019-12-05 16:29:30 +0000

    Progress

--- feedfheconference/lib/View/Screens/statistic.dart
+++ feedfheconference/lib/View/Screens/statistic.dart
@@ -28,6 +28,18 @@ class _StatisticsFormState extends State<StatisticsForm> {
   }
 }
 
+List<Response> questionResponses(int questionId) {
+  @override
+  List<Response> responsesList = new List();
+
+  for (int j = 0; j < db.responseList.length; j++) {
+    if(db.responseList[j].questionId == questionId) {
+      responsesList.add(db.responseList[j]);
+    }
+  }
+  return responsesList;
+}
+
 List<Widget> listMyQuestions(var formId, String talkName) {
   @override
   List<Widget> listQuestion = new List();
@@ -41,7 +53,7 @@ List<Widget> listMyQuestions(var formId, String talkName) {
         for(int j = 0; j < db.formQuestionList.length; j++){
           if(form.listIdFormQuestions[h] == db.formQuestionList[j].id){
             index++;
-            listQuestion.add(QuestionText(db.formQuestionList[j].questionText, j, talkName, index));
+            listQuestion.add(QuestionText(db.formQuestionList[j].questionText, form.listIdFormQuestions[h], talkName, index));
             break;
           }
         }
@@ -63,12 +75,14 @@ class QuestionText extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       child: Container(
+          color: Colors.grey[200],
+          padding: const EdgeInsets.all(10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Text(
-                questionText,
-                style: TextStyle(fontSize: 22),
+                index.toString() + ". " + questionText,
+                style: TextStyle(fontSize: 19),
               )
             ],
           ),
@@ -96,22 +110,77 @@ NestedScrollView buildStatisticsForm(
     headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
       return <Widget>[
         new SliverAppBar(
-          title: new Text(talkName + '\nQuestions'),
+          title: new Text('Form statistics'),
+          bottom: new TabBar(tabs: [Tab(text: "Questions"), Tab(text: "General stats")],
+          controller: _tabController),
         )
       ];
     },
-    body: new Scaffold(
-      body: Container(
-          margin: const EdgeInsets.all(20.0),
-          //color: Colors.amber[600],
-          child: SingleChildScrollView(
-              padding: const EdgeInsets.all(6),
-              child: new Column(
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                  children: listMyQuestions(formId, talkName)
-              )
-          )
-      ),
-    ),
+    body: new TabBarView(
+      children: <Widget>[
+        QuestionsListPage(formId, talkName),
+        GeneralStats(formId)
+      ],
+    )
   );
 }
+
+class QuestionsListPage extends StatelessWidget {
+  final int formId;
+  final String talkName;
+
+  @override
+  QuestionsListPage(this.formId, this.talkName);
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: const EdgeInsets.all(20.0),
+        //color: Colors.amber[600],
+        child: SingleChildScrollView(
+            padding: const EdgeInsets.all(6),
+            child: new Column(
+                children: listMyQuestions(formId, talkName)
+            )
+        )
+    );
+  }
+}
+
+class GeneralStats extends StatelessWidget {
+  final int formId;
+
+   @override
+   GeneralStats(this.formId);
+   Widget build(BuildContext context) {
+     List<List<Response>> responses = new List();
+     FormTalk form;
+
+     for(int i = 0; i < db.formList.length; i++) {
+       if(formId == db.formList[i].id) {
+         form = db.formList[i];
+         for(int j = 0; j < form.listIdFormQuestions.length; j++) {
+           responses.add(questionResponses(form.listIdFormQuestions[j]));
+         }
+       }
+     }
+
+     FormQuestion lessRespondedQuestion;
+     int lessRespondedQuestionId;
+
+     for(int i = 0; i < responses.length; i++) {
+       if(i == 0)
+         lessRespondedQuestionId = form.listIdFormQuestions[i];
+
+       else if(responses[i].length < responses[lessRespondedQuestionId].length)
+         lessRespondedQuestionId = form.listIdFormQuestions[i];
+
+     }
+
+     for(int i = 0; i < db.formQuestionList.length; i++) {
+       if(db.formQuestionList[i].id == lessRespondedQuestionId)
+         lessRespondedQuestion = db.formQuestionList[i];
+     }
+
+    return new Text(lessRespondedQuestion.questionText);
+  }
+}
+
